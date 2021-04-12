2ad5f602b49b08155197d6f4b6243a7dee09a736
Manish Sharma <Manish.Sharma2@Staples.com>
2021-04-12 12:18:28 +0530

    User Modal code refactored

--- projects/task-manager/src/app/modules/tasks-manager/users/users.component.ts
+++ projects/task-manager/src/app/modules/tasks-manager/users/users.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Users } from '../../../interfaces/user.interface';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  @Input() users: Users[];
+  @Output() selectUser = new EventEmitter<any>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * Emitting select event
+   * @param userId: number
+   */
+  emitSelectUserEvent(userId: number): void {
+    this.selectUser.emit(userId);
   }
 
 }
